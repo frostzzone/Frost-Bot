9806163883a809e68080ede8d895906c7ad0b0c0
frostzzone <65735427+frostzzone@users.noreply.github.com>
2021-12-07 16:03:30 -0800

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -500,50 +500,122 @@ console.log('Logged in as "'+ s4d.client.user.username + '"')
             }
         }
 
+        
         if (command == 'bal') {
-            if (s4d.database.has(String((String((s4dmessage.author).id) + '-cash')))) {
-                s4dmessage.reply({
-                    embeds: [{
-                        title: (String((s4dmessage.author).username) + '\'s balance'),
-                        color: null,
-                        image: {
-                            url: null
-                        },
-                        description: ('**Cash**: ' + String(s4d.database.get(String((String((s4dmessage.author).id) + '-cash'))))),
-                        footer: {
-                            text: null
-                        },
-                        thumbnail: {
-                            url: null
-                        }
-                    }],
-                    
-                    allowedMentions: {
-                        repliedUser: true
-                    }
-                });
-            } else {
-                s4dmessage.reply({
-                    embeds: [{
-                        title: (String((s4dmessage.author).username) + '\'s balance'),
-                        color: null,
-                        image: {
-                            url: null
-                        },
-                        description: 'You have never played before',
-                        footer: {
-                            text: null
-                        },
-                        thumbnail: {
-                            url: null
-                        }
-                    }],
-                    
-                    allowedMentions: {
-                        repliedUser: true
-                    }
-                });
+            if (!s4d.database.has(String((String((s4dmessage.author).id) + '-bank')))) {
+                s4d.database.set(String((String((s4dmessage.author).id) + '-bank')), 0);
             }
+            if (!s4d.database.has(String((String((s4dmessage.author).id) + '-cash')))) {
+                s4d.database.set(String((String((s4dmessage.author).id) + '-cash')), 0);
+            }
+            try {
+                if (!((s4dmessage.mentions.members.first().user) == null)) {
+                    if (!(s4d.database.get(String((String((s4dmessage.mentions.members.first().user).id) + '-cash'))) == null) && !(String((s4dmessage.mentions.members.first().user).id) + '-bank' == null)) {
+                        s4dmessage.reply({
+                            embeds: [new MessageEmbed()
+                                .setTitle(String((['**', (s4dmessage.mentions.members.first().user).username, '\'s Balance**'].join(''))))
+
+
+                                .setDescription(String((['**Wallet**:', s4d.database.get(String((String((s4dmessage.mentions.members.first().user).id) + '-cash'))), '\n', '**Bank**:', s4d.database.get(String((String((s4dmessage.mentions.members.first().user).id) + '-bank')))].join(''))))
+
+
+                            ],
+                            ,
+                            allowedMentions: {
+                                repliedUser: true
+                            }
+                        });
+                    } else {
+                        s4dmessage.reply({
+                            embeds: [new MessageEmbed()
+                                .setTitle(String((String((s4dmessage.mentions.members.first().user).username) + '\'s Balance')))
+
+
+                                .setDescription(String('This user has never played'))
+
+
+                            ],
+                            ,
+                            allowedMentions: {
+                                repliedUser: true
+                            }
+                        });
+                    }
+                }
+
+            } catch (err) {
+                command = arguments2.splice(0, 1)[0];
+                try {
+                    if (!!command.length) {
+                        if (!(s4d.database.get(String((String(Number(command)) + '-cash'))) == null) && !(String(Number(command)) + '-bank' == null)) {
+                            s4dmessage.reply({
+                                embeds: [new MessageEmbed()
+                                    .setTitle(String((['**', (((s4dmessage.guild).members.cache.get(command) || await (s4dmessage.guild).members.fetch(command)).user).username, '\'s Balance**'].join(''))))
+
+
+                                    .setDescription(String((['**Wallet**:', s4d.database.get(String((String(Number(command)) + '-cash'))), '\n', '**Bank**:', s4d.database.get(String((String(Number(command)) + '-bank')))].join(''))))
+
+
+                                ],
+                                ,
+                                allowedMentions: {
+                                    repliedUser: true
+                                }
+                            });
+                        } else {
+                            s4dmessage.reply({
+                                embeds: [new MessageEmbed()
+                                    .setTitle(String((String((((s4dmessage.guild).members.cache.get(command) || await (s4dmessage.guild).members.fetch(command)).user).username) + '\'s Balance')))
+
+
+                                    .setDescription(String('This user has never played'))
+
+
+                                ],
+                                ,
+                                allowedMentions: {
+                                    repliedUser: true
+                                }
+                            });
+                        }
+                    }
+
+                } catch (err) {
+                    if (!(s4d.database.get(String((String((s4dmessage.author).id) + '-cash'))) == null) && !(String((s4dmessage.author).id) + '-bank' == null)) {
+                        s4dmessage.reply({
+                            embeds: [new MessageEmbed()
+                                .setTitle(String((['**', (s4dmessage.author).username, '\'s Balance**'].join(''))))
+
+
+                                .setDescription(String((['**Wallet**:', s4d.database.get(String((String((s4dmessage.author).id) + '-cash'))), '\n', '**Bank**:', s4d.database.get(String((String((s4dmessage.author).id) + '-bank')))].join(''))))
+
+
+                            ],
+                            ,
+                            allowedMentions: {
+                                repliedUser: true
+                            }
+                        });
+                    } else {
+                        s4dmessage.reply({
+                            embeds: [new MessageEmbed()
+                                .setTitle(String((String((s4dmessage.author).username) + '\'s Balance')))
+
+
+                                .setDescription(String('This user has never played'))
+
+
+                            ],
+                            ,
+                            allowedMentions: {
+                                repliedUser: true
+                            }
+                        });
+                    }
+
+                };
+
+            };
         }
  
 
